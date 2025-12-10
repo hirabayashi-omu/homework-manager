7c2bd2aaf7053665d5f6c505173b89842d2f14f4
hirabayashi-omu <hirabayashi@omu.ac.jp>
2025-12-10 21:20:48 +0900

    Update homework_app.py

--- homework_app.py
+++ homework_app.py
@@ -348,12 +348,12 @@ with tabs[1]:
             
                 # 4列目: 完了ボタン
                 with cols[3]:
-                    if st.button("完了にする", key=f"done_{int(row['id'])}"):
+                    if st.button(f"完了にする", key=f"done_{int(row['id'])}"):
                         st.session_state.done_id = row["id"]
             
                 # 5列目: 削除ボタン
                 with cols[4]:
-                    if st.button("削除", key=f"del_{int(row['id'])}")
+                    if st.button(f"削除", key=f"del_{int(row['id'])}")
                         st.session_state.delete_id = row["id"]
             
             # -----------------------------
@@ -408,6 +408,7 @@ with tabs[1]:
 
 st.markdown("---")
 st.caption("※ Google Drive API による完全クラウド永続化版アプリです")
+
 
 
 
